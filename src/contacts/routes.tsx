84eb740267ee98beb2becea8b998b248c1695c2b
hazqeelknight <hazqeel.knight@gmail.com>
2025-09-09 21:02:08 +0500

    Updated routes.tsx

--- src/contacts/routes.tsx
+++ src/contacts/routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// Placeholder components - to be implemented by the contacts module developer
+// Lazy load all contact pages
 const ContactsOverview = React.lazy(() => import('./pages/ContactsOverview'));
 const ContactsList = React.lazy(() => import('./pages/ContactsList'));
+const ContactDetail = React.lazy(() => import('./pages/ContactDetail'));
 const ContactGroups = React.lazy(() => import('./pages/ContactGroups'));
+const ContactGroupDetail = React.lazy(() => import('./pages/ContactGroupDetail'));
 const ContactInteractions = React.lazy(() => import('./pages/ContactInteractions'));
 
 const ContactsRoutes: React.FC = () => {
@@ -12,7 +14,9 @@ const ContactsRoutes: React.FC = () => {
     <Routes>
       <Route index element={<ContactsOverview />} />
       <Route path="list" element={<ContactsList />} />
+      <Route path="detail/:id" element={<ContactDetail />} />
       <Route path="groups" element={<ContactGroups />} />
+      <Route path="groups/:id" element={<ContactGroupDetail />} />
       <Route path="interactions" element={<ContactInteractions />} />
     </Routes>
   );
